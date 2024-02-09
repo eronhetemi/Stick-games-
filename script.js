13b59e56412f14afc7c0c712871ec9db136236c0
eronhetemi <ehetemi23@gmail.com>
2024-02-09 22:49:49 +0100

    5

--- script.js
+++ script.js
@@ -57,3 +57,186 @@ canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 
 const introductionElement = document.getElementById("introduction");
+
+const perfectElement = document.getElementById("perfect");
+const restartButton = document.getElementById("restart");
+const scoreElement = document.getElementById("score");
+
+// Initialize layout
+resetGame();
+
+// Resets game variables and layouts but does not start the game (game starts on keypress)
+function resetGame() {
+  // Reset game progress
+  phase = "waiting";
+  lastTimestamp = undefined;
+  sceneOffset = 0;
+  score = 0;
+
+  introductionElement.style.opacity = 1;
+  perfectElement.style.opacity = 0;
+  restartButton.style.display = "none";
+  scoreElement.innerText = score;
+
+  // The first platform is always the same
+  // x + w has to match paddingX
+  platforms = [{ x: 50, w: 50 }];
+  generatePlatform();
+  generatePlatform();
+  generatePlatform();
+  generatePlatform();
+
+  sticks = [{ x: platforms[0].x + platforms[0].w, length: 0, rotation: 0 }];
+
+  trees = [];
+  generateTree();
+  generateTree();
+  generateTree();
+  generateTree();
+  generateTree();
+  generateTree();
+  generateTree();
+  generateTree();
+  generateTree();
+  generateTree();
+
+  heroX = platforms[0].x + platforms[0].w - heroDistanceFromEdge;
+  heroY = 0;
+
+  draw();
+}
+
+function generateTree() {
+  const minimumGap = 30;
+  const maximumGap = 150;
+
+  // X coordinate of the right edge of the furthest tree
+  const lastTree = trees[trees.length - 1];
+  let furthestX = lastTree ? lastTree.x : 0;
+
+  const x =
+    furthestX +
+    minimumGap +
+    Math.floor(Math.random() * (maximumGap - minimumGap));
+
+  const treeColors = ["#6D8821", "#8FAC34", "#98B333"];
+  const color = treeColors[Math.floor(Math.random() * 3)];
+
+  trees.push({ x, color });
+}
+
+function generatePlatform() {
+  const minimumGap = 40;
+  const maximumGap = 200;
+  const minimumWidth = 20;
+  const maximumWidth = 100;
+
+  // X coordinate of the right edge of the furthest platform
+  const lastPlatform = platforms[platforms.length - 1];
+  let furthestX = lastPlatform.x + lastPlatform.w;
+
+  const x =
+    furthestX +
+    minimumGap +
+    Math.floor(Math.random() * (maximumGap - minimumGap));
+  const w =
+    minimumWidth + Math.floor(Math.random() * (maximumWidth - minimumWidth));
+
+  platforms.push({ x, w });
+}
+
+resetGame();
+
+// If space was pressed restart the game
+window.addEventListener("keydown", function (event) {
+  if (event.key == " ") {
+    event.preventDefault();
+    resetGame();
+    return;
+  }
+});
+
+window.addEventListener("mousedown", function (event) {
+  if (phase == "waiting") {
+    lastTimestamp = undefined;
+    introductionElement.style.opacity = 0;
+    phase = "stretching";
+    window.requestAnimationFrame(animate);
+  }
+});
+
+window.addEventListener("mouseup", function (event) {
+  if (phase == "stretching") {
+    phase = "turning";
+  }
+});
+
+window.addEventListener("resize", function (event) {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  draw();
+});
+
+window.requestAnimationFrame(animate);
+
+// The main game loop
+function animate(timestamp) {
+  if (!lastTimestamp) {
+    lastTimestamp = timestamp;
+    window.requestAnimationFrame(animate);
+    return;
+  }
+
+  switch (phase) {
+    case "waiting":
+      return; // Stop the loop
+    case "stretching": {
+      sticks.last().length += (timestamp - lastTimestamp) / stretchingSpeed;
+      break;
+    }
+    case "turning": {
+      sticks.last().rotation += (timestamp - lastTimestamp) / turningSpeed;
+
+      if (sticks.last().rotation > 90) {
+        sticks.last().rotation = 90;
+
+        const [nextPlatform, perfectHit] = thePlatformTheStickHits();
+        if (nextPlatform) {
+          // Increase score
+          score += perfectHit ? 2 : 1;
+          scoreElement.innerText = score;
+
+          if (perfectHit) {
+            perfectElement.style.opacity = 1;
+            setTimeout(() => (perfectElement.style.opacity = 0), 1000);
+          }
+
+          generatePlatform();
+          generateTree();
+          generateTree();
+        }
+
+        phase = "walking";
+      }
+      break;
+    }
+    case "walking": {
+      heroX += (timestamp - lastTimestamp) / walkingSpeed;
+
+      const [nextPlatform] = thePlatformTheStickHits();
+      if (nextPlatform) {
+        // If hero will reach another platform then limit it's position at it's edge
+        const maxHeroX = nextPlatform.x + nextPlatform.w - heroDistanceFromEdge;
+        if (heroX > maxHeroX) {
+          heroX = maxHeroX;
+          phase = "transitioning";
+        }
+      } else {
+        // If hero won't reach another platform then limit it's position at the end of the pole
+        const maxHeroX = sticks.last().x + sticks.last().length + heroWidth;
+        if (heroX > maxHeroX) {
+          heroX = maxHeroX;
+          phase = "falling";
+        }
+      }
+      break;
