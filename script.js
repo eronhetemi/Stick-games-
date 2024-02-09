18cccb3975240495b88b23cb59ba82f367f25156
eronhetemi <ehetemi23@gmail.com>
2024-02-09 15:27:25 +0100

    js

---
+++ script.js
@@ -0,0 +1,59 @@
+// Extend the base functionality of JavaScript
+Array.prototype.last = function () {
+  return this[this.length - 1];
+};
+
+// A sinus function that acceps degrees instead of radians
+Math.sinus = function (degree) {
+  return Math.sin((degree / 180) * Math.PI);
+};
+
+// Game data
+let phase = "waiting"; // waiting | stretching | turning | walking | transitioning | falling
+let lastTimestamp; // The timestamp of the previous requestAnimationFrame cycle
+
+let heroX; // Changes when moving forward
+let heroY; // Only changes when falling
+let sceneOffset; // Moves the whole game
+
+let platforms = [];
+let sticks = [];
+let trees = [];
+
+// Todo: Save high score to localStorage (?)
+
+let score = 0;
+
+// Configuration
+const canvasWidth = 375;
+const canvasHeight = 375;
+const platformHeight = 100;
+const heroDistanceFromEdge = 10; // While waiting
+const paddingX = 100; // The waiting position of the hero in from the original canvas size
+const perfectAreaSize = 10;
+
+// The background moves slower than the hero
+const backgroundSpeedMultiplier = 0.2;
+
+const hill1BaseHeight = 100;
+const hill1Amplitude = 10;
+const hill1Stretch = 1;
+const hill2BaseHeight = 70;
+const hill2Amplitude = 20;
+const hill2Stretch = 0.5;
+
+const stretchingSpeed = 4; // Milliseconds it takes to draw a pixel
+const turningSpeed = 4; // Milliseconds it takes to turn a degree
+const walkingSpeed = 4;
+const transitioningSpeed = 2;
+const fallingSpeed = 2;
+
+const heroWidth = 17; // 24
+const heroHeight = 30; // 40
+const canvas = document.getElementById("game");
+canvas.width = window.innerWidth; // Make the Canvas full screen
+canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext("2d");
+
+const introductionElement = document.getElementById("introduction");
